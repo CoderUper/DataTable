48db09f58815aad49a136f06b336b23e9b18aa32
CoderUper <jinzhuguang08@163.com>
2019-10-27 21:59:35 +0800

    1

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "main.h"
+int main()
+{
+    Menu();
+    GetCommand();
+    return 0;
+}
+
---
+++ main.h
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+//打印菜单
+void Menu()
+{
+    std::cout<<"------------------------  Linux环境高级编程作业------------------------\n";
+    std::cout<<"    1.插入数据：insert xxx,xxxx,xxxx,xxx,...  \n ";
+    std::cout<<"    2.查找数据：select on 属性 where 属性>=xx and 属性<=xx(范围查找)\n";
+    std::cout<<"    3.建立索引：craate index on 属性\n";
+    std::cout<<"    4.退出：exit\n    ";
+    std::cout<<"                                      by 金祝光 201922081133\n";
+}
+
+
+//获取用户指令
+void GetCommand()
+{
+    std::string command;
+    std::cout<<"DataTable >";
+    std::cin>>command;
+    while(command!="exit")
+    {
+        std::cout<<command<<std::endl;        
+        std::cout<<"DataTable >";
+        std::cin>>command;
+    }
+    std::cout<<"再见！\n";
+}

fc840bb1e03d019510928cfe06182632cd10365a
CoderUper <jinzhuguang08@163.com>
2019-10-29 11:51:06 +0800

    第二次提交

--- DataTable.cpp
+++ DataTable.cpp
@@ -1,12 +1,75 @@
 #include "DataTable.h"
 
+//instance_mutex初始化
+pthread_mutex_t DataTable::instance_mutex=PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t DataTable::data_rows_mutex=PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t DataTable::recent_file_mutex=PTHREAD_MUTEX_INITIALIZER;
+
 DataTable::DataTable(uint _max_file_num,uint _max_file_data_rows,uint _data_rows):max_file_num(_max_file_num),max_file_data_rows(_max_file_data_rows),data_rows(_data_rows)
 {
-    pthread_mutex_init(&instance_mutex,NULL);
-
+    memset(recent_file_status,0,sizeof(recent_file_status));
 }
-int DataTable::data_rows=0;
 
-DataTable* DataTable::data_instance=NULL;
-std::vector<int> DataTable::all_file;
-std::map<int,std::fstream&> DataTable::recent_file;
+uint DataTable::get_max_rows() const
+{
+    return max_rows;
+}
+
+uint DataTable::get_max_file_num() const
+{
+    return max_file_num;
+}
+
+uint DataTable::get_max_file_data_rows() const
+{
+    return max_file_data_rows;
+}
+
+void DataTable::lock(pthread_mutex_t* mutex)
+{
+    pthread_mutex_lock(mutex);
+}
+
+void DataTable::unlock(pthread_mutex_t* mutex)
+{
+    pthread_mutex_unlock(mutex);
+}
+
+
+//单例模式，获取对象实例
+DataTable* DataTable::get_instance()
+{
+    if(data_instance==NULL)
+    {
+        lock(&instance_mutex);
+        if(data_instance==NULL)
+        {
+            data_instance=new DataTable(1000,1000);
+        }
+        unlock(&instance_mutex);
+    }
+    return data_instance;
+}
+
+//查询数据
+std::vector<int>  DataTable::search(KeyType key,int col,int minimum,int maximum)
+{
+    //查看是否为这列建立索引
+    //如果有，用B+树搜索
+    //没有的话
+    //用B+树索引开始搜索
+    //最近打开文件没有搜索到，搜索
+}
+
+//向某一列插入一行数据
+void DataTable::insert(std::vector<KeyType> row)
+{
+    //先找最近打开文件
+    lock(&recent_file_mutex);
+    for(int i=0;i<max_recent_file;i++)
+    {
+        if(recent_file[i]&&)
+    }
+    //若没满，被占用则插入
+    //若满了，或者被占用则插入磁盘中的文件
+}
--- DataTable.h
+++ DataTable.h
@@ -4,28 +4,40 @@
 #include <string>
 #include <map>
 #include <fstream>
+#include <climits>
+#include <cstring>
 class DataTable
 {
 public:
-    DataTable(uint _max_file_num,uint _max_file_data_rows,uint _data_rows);
+    DataTable(uint _max_file_num,uint _max_file_data_rows,uint _data_rows=0);
     ~DataTable();
     uint get_max_rows() const;
     uint get_max_file_num() const;
     uint get_max_file_data_rows() const;
     static DataTable* get_instance();
-    void instance_lock();
-    void instance_unlock();
+    static void instance_mutex_init();
     typedef int64_t KeyType;
+    std::vector<int> search(KeyType key,int col=1,int minimum=LONG_MIN,int maximum=LONG_MAX);
+    void insert(std::vector<KeyType> row);
+
 private:
+    static void lock(pthread_mutex_t* mutex);
+    static void unlock(pthread_mutex_t* mutex);   
+    
     const uint max_rows=1000000;   //最大数据量
     const uint max_file_num;  //最大文件数
     const uint max_file_data_rows;   //每个文件最大数据量
+    const static uint max_recent_file=5;
     uint data_rows;   //目前的数据行
     static DataTable* data_instance;         //Table对象实例
     std::vector<int> all_file;    //存储所有文件,以序号标识
+
     std::map<int,std::fstream&> recent_file;   //最近的打开文件表
-    pthread_mutex_t instance_mutex;     //数据锁
-    std::vector< pthread_mutex_t> file_mutex;
+    static pthread_mutex_t recent_file_mutex;
+    int recent_file_status[max_recent_file];
+
+    static pthread_mutex_t instance_mutex;     //数据锁
+    static pthread_mutex_t data_rows_mutex;
     std::vector<pthread_mutex_t> recent_mutex;
 };
 
